f3604db7af008dacbc6849e3b946964ece3bd9e6
unknown <vnsmusyoki@gmail.com>
2022-02-26 12:04:00 +0300

    login page

---
+++ header.php
@@ -0,0 +1,89 @@
+    <!--Top Header-->
+    <div class="top-header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-10 col-sm-8 col-md-5 col-lg-4">
+                    <div class="currency-picker">
+
+                    </div>
+                    <div class="language-dropdown">
+
+                    </div>
+                    <p class="phone-no"><i class="anm anm-phone-s"></i> Now open for Deals with you</p>
+                </div>
+                <div class="col-sm-4 col-md-4 col-lg-4 d-none d-lg-none d-md-block d-lg-block">
+                    <div class="text-center">
+                        <p class="top-header_middle-text"> Free Shipping</p>
+                    </div>
+                </div>
+                <div class="col-2 col-sm-4 col-md-3 col-lg-4 text-right">
+                    <span class="user-menu d-block d-lg-none"><i class="anm anm-user-al" aria-hidden="true"></i></span>
+                    <ul class="customer-links list-inline">
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="register.php">Create Account</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End Top Header-->
+    <!--Header-->
+    <div class="header-wrap classicHeader animated d-flex">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <!--Desktop Logo-->
+                <div class="logo col-md-2 col-lg-2 d-none d-lg-block">
+                    <a href="index.php">
+                        <img src="assets/images/logo.svg" alt="Sales Inventory" title="Sales Inventory" />
+                    </a>
+                </div>
+                <!--End Desktop Logo-->
+                <div class="col-2 col-sm-3 col-md-3 col-lg-8">
+                    <div class="d-block d-lg-none">
+                        <button type="button" class="btn--link site-header__menu js-mobile-nav-toggle mobile-nav--open">
+                            <i class="icon anm anm-times-l"></i>
+                            <i class="anm anm-bars-r"></i>
+                        </button>
+                    </div>
+                    <!--Desktop Menu-->
+                    <nav class="grid__item" id="AccessibleNav">
+                        <!-- for mobile -->
+                        <ul id="siteNav" class="site-nav medium center hidearrow">
+                            <li class="lvl1 parent megamenu"><a href="index.php">Home </a> </li>
+                            <li class="lvl1 parent megamenu"><a href="#">Shop </a> </li>
+                            <li class="lvl1 parent megamenu"><a href="#">Product </a> </li>
+                            <li class="lvl1 parent dropdown"><a href="#">Pages <i class="anm anm-angle-down-l"></i></a>
+                                <ul class="dropdown">
+
+                                    <li><a href="login.php" class="site-nav">Login</a></li>
+                                    <li><a href="register.php" class="site-nav">Create New Account</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="lvl1"><a href="#"><b>Contact us!</b> <i class="anm anm-angle-down-l"></i></a></li>
+                        </ul>
+                    </nav>
+                    <!--End Desktop Menu-->
+                </div>
+                <!--Mobile Logo-->
+                <div class="col-6 col-sm-6 col-md-6 col-lg-2 d-block d-lg-none mobile-logo">
+                    <div class="logo">
+                        <a href="index.php">
+                            <img src="assets/images/logo.svg" alt="Sales Inventory System" title="Sales Inventory System" />
+                        </a>
+                    </div>
+                </div>
+                <!--Mobile Logo-->
+                <div class="col-4 col-sm-3 col-md-3 col-lg-2">
+
+                    <div class="site-header__search">
+                        <button type="button" class="search-trigger"><i class="icon anm anm-search-l"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End Header-->
+    <!--Mobile Menu-->
+     
+    <!--End Mobile Menu-->
