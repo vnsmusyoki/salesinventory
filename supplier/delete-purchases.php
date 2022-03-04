e3a67b195ffc2872d84d345ef2463c748438d00d
Brenda Wanjiru <vnsmusyoki@gmail.com>
2022-03-04 03:53:48 +0300

    clean supplier

--- supplier/delete-purchases.php
+++
@@ -1,8 +0,0 @@
-<?php include 'admin.php'; ?>
-<?php
-$product = $_GET['purchases'];
-$checkstation = "DELETE  FROM `purchases` WHERE `purchases_id` = '$product'";
-$querycheckstation = mysqli_query($conn, $checkstation);
-if ($querycheckstation) {
-    echo "<script>window.location.replace('all-purchases.php');</script>";
-}
